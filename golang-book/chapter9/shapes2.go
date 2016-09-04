76580e85b04169c789c267689024462000806b29
kohrVid <kohrVid@zoho.com>
2016-09-04 20:22:02 +0100

    Have tried implementing interfaces again after reading through tour.golang.org

--- golang-book/chapter9/shapes2.go
+++ golang-book/chapter9/shapes2.go
@@ -26,11 +26,11 @@ type Circle struct {
 	x, y, r float64
 }
 
-func (c *Circle) area() float64 {
+func (c Circle) area() float64 {
 	return math.Pi * math.Pow(c.r, 2)
 }
 
-func (c *Circle) perimeter() float64 {
+func (c Circle) perimeter() float64 {
 	return math.Pi * 2 * c.r
 }
 
@@ -38,21 +38,21 @@ type Rectangle struct {
 	x1, y1, x2, y2 float64
 }
 
-func (r *Rectangle) area() float64 {
+func (r Rectangle) area() float64 {
 	l := distance(r.x1, r.y1, r.x1, r.y2)
 	w := distance(r.x1, r.y1, r.x2, r.y1)
 	return l * w
 }
-func (r *Rectangle) perimeter() float64 {
+func (r Rectangle) perimeter() float64 {
 	l := distance(r.x1, r.y1, r.x1, r.y2)
 	w := distance(r.x1, r.y1, r.x2, r.y1)
 	return (2 * l) + (2 * w)
 }
 
 func main() {
-	c := Circle{x: 10, y: 5, r: 5}
-	r := Rectangle{0, 0, 10, 10}
-	fmt.Println(totalArea(&c, &r))
+	var c Shape = Circle{x: 10, y: 5, r: 5}
+	var r Shape = Rectangle{0, 0, 10, 10}
+	fmt.Println(totalArea(c, r))
 	fmt.Printf("The perimeter of the circle is %.2fcm\n", c.perimeter())
 	fmt.Printf("The perimeter of the rectangle is %.2fcm\n", r.perimeter())
 }
